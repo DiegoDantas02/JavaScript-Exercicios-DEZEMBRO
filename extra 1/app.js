b31220ae5b1e7f4f844918df428f3dbd4fa6585d
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2022-11-29 22:15:13 -0300

    estrutura de decisão

---
+++ extra 1/app.js
@@ -0,0 +1,28 @@
+// 1- Faça um programa que verifique se uma pessoa é maior de idade. A pessoa deve informar a idade, se ela tiver 18 anos ou mais, então o programa deve escrever que ela já pode dirigir, se não tiver, o programa deve informar pra ela voltar mais tarde.
+// function maiordeidade(){
+//     let idade = document.getElementById("idade").value
+//     if(idade >=18){
+//         document.getElementById("resposta").innerHTML = "Você já pode dirigir! ! !"
+
+//     }else{
+//     document.getElementById("resposta").innerHTML = "Volte mais tarde! ! !"
+// }
+// }
+
+// 2- - Faça um programa que verifique se média de um aluno é maior ou igual a 5, se for mostre "Aprovado" caso contrário, mostre "reprovado". Para se calcular a média deve-se informar 3 notas.
+
+function Média(){
+
+    let Nota1 = parseFloat document.getElementById("Nota1").value
+    let Nota2 = parseFloat document.getElementById("Nota2").value
+    let Nota3 = parseFloat document.getElementById("Nota3").value
+}
+
+
+
+
+
+
+
+
+// 3- Faça um programa que verifique se a média de um aluno é maior ou igual a 7, se for, mostre a mensagem "Aprovado",se a média for maior ou igual a 5, mostre a mensagem "Recuperação", caso contrário mostre a mensagem "Reprovado". Para calcular a média, deve-se informar 3 notas.
