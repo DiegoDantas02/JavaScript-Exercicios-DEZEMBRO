c001a3d96d0eddaeb0e83f30f762f8d0a347ebe5
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2022-12-09 20:14:57 -0300

    ,

--- exercicio11/app.js
+++ exercicio11/app.js
@@ -2,6 +2,6 @@ function impar(){
     let contador = 0
     while(contador += 100){
     document.getElementById("resposta").innerHTML += contador + " "
-    contador + contador + 1
+    contador + contador +1
 }
 }
