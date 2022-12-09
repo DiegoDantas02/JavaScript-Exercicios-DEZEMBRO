8da2ea6ddbc7e4efa30f484779ae668521e2367c
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2022-12-09 19:35:06 -0300

    .

--- exercicio10/app.js
+++ exercicio10/app.js
@@ -1,2 +1,8 @@
 //nmrs pares entre 0 e 100
-
+function par(){
+    let contador = 0
+    while(contador += 100){
+    document.getElementById("resposta").innerHTML += contador + " "
+    contador + contador + 2
+}
+}
