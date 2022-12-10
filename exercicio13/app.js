088770a7b4728c6c87c723b6a7de11c725287fde
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2022-12-10 13:07:25 -0300

    .

---
+++ exercicio13/app.js
@@ -0,0 +1,12 @@
+function maiorMenor(){
+    document.getElementById("num").innerHTML
+   
+    if  ("<= 18" ){
+        write("Você é de menor! ! !")
+
+        else if (">= 18 "){
+            write("Você é de maior! ! !")
+        }
+
+    }
+}
